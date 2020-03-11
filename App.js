51f9ce489a920de3561c045ef00ea6c516619401
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-03-11 14:17:43 -0700

    Load fonts in to App.

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import Home from './screens/home';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+import StackNavigator from './routes/drawer';
+
+/* This is how to create a function like componentDidMount in a functional component instead of a class*/
+const getFonts = () => Font.loadAsync({
+  'groovy': require('./assets/fonts/Corben-Bold.ttf'),
+  'curved-bold': require('./assets/fonts/Coustard-Black.ttf')
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+  const [fontsLoaded, setFontsLoaded] = useState(false);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  /*Here is where we check to see if the state of font is loaded*/
+  if (fontsLoaded) {
+    return (
+      <StackNavigator />
+    );
+  } else {
+    return (
+      /*Here is where you call on the AppLoading feature of expo and call on an async funciton of getFonts()*/
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+      />
+    )
+  }
+
+}
