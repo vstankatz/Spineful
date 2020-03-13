52b5aa45591139cadf1fd0b4dcb5b6248c22ef10
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-03-12 18:49:19 -0700

    Add style to individual stretches.

--- screens/stretches.js
+++ screens/stretches.js
@@ -3,25 +3,49 @@ import { StyleSheet, Text, View, TouchableOpacity, FlatList, Keyboard, Button } 
 import { globalStyles } from '../styles/global';
 
 export default function Stretches({ navigation }) {
-    const [position, setPostion] = useState({})
-
     const positionNumber = (navigation.getParam('position').length)
+    const [position, setPostion] = useState({positionNumber})
     
-    let (navigation.getParam('position').length) = position
-
-    const instructions = ( navigation.getParam("contents")).map(content);
+    
+    
+    const instructions = ( navigation.getParam('contents'))
+    // .map(contnt);
     return (
         <View style={globalStyles.container}>
-            <Text>{navigation.getParam("title")}</Text>
+            <View style={styles.textBox}>
+                <Text style={styles.title}>{navigation.getParam("title")}</Text>
+            </View>
             <FlatList 
                 data={instructions}
-                renderItem={({item}) =>  (
-                <Text>{item}</Text>
+                scrollEnabled={false}
+                renderItem={({ item, index }) => (
+                
+                <Text style={styles.list}>{index+1}. {item}</Text>
             )}
             />
-            <Text>
-        {navigation.getParam("footer")}, ep.{navigation.getParam("season")}
+            <Text style={styles.footer}>
+            {navigation.getParam("footer")}
             </Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    list: {
+        padding:10,
+        fontSize:15
+    },
+    title: {
+        fontFamily: 'groovy',
+        fontSize: 20,
+        alignItems: 'center',
+        textAlign: 'center',
+        color: 'purple'
+
+    },
+    footer: {
+        paddingLeft: 20,
+        paddingRight: 20,
+        fontSize: 10
+    }
+})
