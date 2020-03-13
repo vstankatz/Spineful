2ac96fc5349fda6303c85abd3636baacce1ec193
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-03-12 18:42:41 -0700

    Add global style sheet.

---
+++ styles/global.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+    container: {
+        padding: 24,
+    },
+    title: {
+        fontSize: 18,
+    },
+    paragraph: {
+        marginVertical: 8,
+        lineHeight: 20,
+        fontSize: 18,
+        textAlign: 'center',
+        color: 'dimgrey'
+    },
+    
+})
