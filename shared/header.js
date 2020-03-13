16365aa1ed92cba4e209e18adebe38011db618a0
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-03-12 18:43:35 -0700

    Update header to style based on title.

--- shared/header.js
+++ shared/header.js
@@ -8,6 +8,41 @@ export default function Header({ navigation, title }) {
         navigation.openDrawer()
     }
 
+    const styles = StyleSheet.create({
+        header: {
+            width: '100%',
+            height: '100%',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        icon: {
+            position: 'absolute',
+            right: -50,
+        },
+        headerText: {
+
+        },
+
+    })
+
+    if (title === 'Stretches' | title === 'Stretch' ) {
+        
+         styles.headerText = {
+                fontFamily: 'groovy',
+                fontSize: 25,
+                color: 'pink',
+            
+        }
+    } else {
+        styles.headerText = {
+            fontFamily: 'curved-bold',
+            fontSize: 30,
+            color: 'pink',
+
+        }
+    }
+
     return (
         <View style={styles.header}>
             <MaterialIcons name='menu' size={28} onPress={openMenu} style={styles.icon} />
@@ -18,21 +53,3 @@ export default function Header({ navigation, title }) {
     );
 }
 
-const styles = StyleSheet.create({
-    header: {
-        width: '100%',
-        height: '100%',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    icon: {
-        position: 'absolute',
-        right: -50,
-    },
-    headerText: {
-        fontFamily: 'curved-bold',
-        size: 30,
-        color: 'pink',
-    }
-})
